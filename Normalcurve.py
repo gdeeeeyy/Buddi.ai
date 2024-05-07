38534554e8508a78f10b9481281556c8f9f5fda8
gdeeeeyy <gd14405@gmail.com>
2024-05-07 20:18:25 +0530

    normal curve code corrected with greek alphabets and details in graph added

--- Normalcurve.py
+++ Normalcurve.py
@@ -1,6 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+txt1="This plots in the graph have the same position of the peak but different heights and widths as the standard deviation changes and that controls the width and height of the peak"
+txt2="This plots in the graph have the same height and width but the position of the peak changes for different means as mean controls the position of the peak"
+
 def normal_func(x, mean, std):
     y = 1/(std * np.sqrt(2 * np.pi)) * np.exp( - (x - mean)**2 / (2 * std**2)) 
     return y
@@ -14,11 +17,12 @@ def same_mean(m):
     ysm1=normal_func(xsm1,m,s1)
     ysm2=normal_func(xsm2,m,s2)
     ysm3=normal_func(xsm3,m,s3)
-    plt.plot(xsm1, ysm1, label=f"Normal curve with mean {m} and standard deviation {s1}")
-    plt.plot(xsm2, ysm2, label=f"Normal curve with mean {m} and standard deviation {s2}")
-    plt.plot(xsm3, ysm3, label=f"Normal curve with mean {m} and standard deviation {s3}")
+    plt.plot(xsm1, ysm1, label=f"Normal curve with μ={m} and δ={s1}")
+    plt.plot(xsm2, ysm2, label=f"Normal curve with μ={m} and δ={s2}")
+    plt.plot(xsm3, ysm3, label=f"Normal curve with μ={m} and δ={s3}")
     plt.legend()
     plt.title("Normal curve with same mean and different standard deviation")
+    plt.figtext(0.5, 0.01, txt1, wrap=True, horizontalalignment='center', fontsize=8)
     plt.show()
 
 def same_std(s):
@@ -29,15 +33,18 @@ def same_std(s):
     ysm1=normal_func(xsm1,m1,s)
     ysm2=normal_func(xsm2,m2,s)
     ysm3=normal_func(xsm3,m3,s)
-    plt.plot(xsm1, ysm1, label=f"Normal curve with mean {m1} and standard deviation {s}")
-    plt.plot(xsm2, ysm2, label=f"Normal curve with mean {m2} and standard deviation {s}")
-    plt.plot(xsm3, ysm3, label=f"Normal curve with mean {m3} and standard deviation {s}")
+    plt.plot(xsm1, ysm1, label=f"Normal curve with  μ={m1} and δ={s}")
+    plt.plot(xsm2, ysm2, label=f"Normal curve with μ={m2} and δ={s}")
+    plt.plot(xsm3, ysm3, label=f"Normal curve with μ={m3} and δ={s}")
+    plt.figtext(0.5, 0.01, txt2, wrap=True, horizontalalignment='center', fontsize=8)
     plt.legend()
     plt.title("Normal curve with different mean and same standard deviation")
     plt.show()
 
-
+same_mean(0)
 same_std(1)
+
+
 
 #smds
 #dmss
