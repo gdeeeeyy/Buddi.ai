c0c6de709c5153cb1180f5d496c22bc93174605e
gdeeeeyy <gd14405@gmail.com>
2024-05-07 22:16:34 +0530

    adding user input functionality to the code

--- Normalcurve.py
+++ Normalcurve.py
@@ -10,13 +10,16 @@ def normal_func(x, mean, std):
 
 
 def same_mean(m):
-    s1,s2,s3=1,1.5,2
-    xsm1=np.arange(m-3*s1, m+3*s1, 0.1)
+    s1,s2,s3=1,1.5,2 #different standard deviations
+    #values of x for different values of standard deviation
+    xsm1=np.arange(m-3*s1, m+3*s1, 0.1) 
     xsm2=np.arange(m-3*s2, m+3*s2, 0.1)
     xsm3=np.arange(m-3*s3, m+3*s3, 0.1)
+    #values of y for each points of x
     ysm1=normal_func(xsm1,m,s1)
     ysm2=normal_func(xsm2,m,s2)
     ysm3=normal_func(xsm3,m,s3)
+    #plotting the plots for different values of standard deviation with the same mean 
     plt.plot(xsm1, ysm1, label=f"Normal curve with μ={m} and δ={s1}")
     plt.plot(xsm2, ysm2, label=f"Normal curve with μ={m} and δ={s2}")
     plt.plot(xsm3, ysm3, label=f"Normal curve with μ={m} and δ={s3}")
@@ -24,15 +27,19 @@ def same_mean(m):
     plt.title("Normal curve with same mean and different standard deviation")
     plt.figtext(0.5, 0.01, txt1, wrap=True, horizontalalignment='center', fontsize=8)
     plt.show()
+    plt.close()
 
 def same_std(s):
-    m1,m2,m3=1,1.5,2
+    m1,m2,m3=1,1.5,2 # different means
+    #values of x for different values of mean
     xsm1=np.arange(m1-3*s, m1+3*s, 0.1)
     xsm2=np.arange(m2-3*s, m2+3*s, 0.1)
     xsm3=np.arange(m3-3*s, m3+3*s, 0.1)
+    #values of y for each points of x
     ysm1=normal_func(xsm1,m1,s)
     ysm2=normal_func(xsm2,m2,s)
     ysm3=normal_func(xsm3,m3,s)
+    #plotting the plots for different values of mean with the same standard deviation
     plt.plot(xsm1, ysm1, label=f"Normal curve with  μ={m1} and δ={s}")
     plt.plot(xsm2, ysm2, label=f"Normal curve with μ={m2} and δ={s}")
     plt.plot(xsm3, ysm3, label=f"Normal curve with μ={m3} and δ={s}")
--- Randomsampling.py
+++ Randomsampling.py
@@ -19,3 +19,10 @@ def draw_samples(pmf, no_of_samples):
 
 pmf={"ABCD":0.03703704,"EFGH":0.07407407,"IJKL":0.14814815,"MNOP":0.22222222,"QRST":0.40740741, "UVWXYZ":0.11111111}
 print(draw_samples(pmf, 4))
+
+# pmf={}
+# n=int(input("Enter the number of feature labels: "))
+# for _ in range(n):
+#     feature=input("Enter the feature: ")
+#     value=float(input("Enter the pmf of the feature as a floating point value: "))
+#     pmf[feature]=value
