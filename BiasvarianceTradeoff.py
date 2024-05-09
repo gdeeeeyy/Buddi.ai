Added function signatures, plotted model performance and the bias variance tradeoff plot should be corrected(make sure that the variance is not lower than the bias)

BiasvarianceTradeoff.py
@@ -8,17 +8,18 @@ def populationGenerator(x:int)->int:
 
 X=np.array(np.linspace(-5,5,100))
 n=len(X)
+Y=populationGenerator(X)
 
 split_idx_train=int(X.shape[0]*0.7)
 split_idx_valid=split_idx_train+int(X.shape[0]*0.1)
 
 
-X1_train=X[:split_idx_train]
+X1_train=np.array(X[:split_idx_train])
 X0_train=np.array(X1_train**0)
 X2_train=np.array(X1_train**2)
 X3_train=np.array(X1_train**3)
 X4_train=np.array(X1_train**4)
-Y_train=np.array(populationGenerator(X1_train))
+Y_train=np.array(Y[:split_idx_train])
 
 
 X1_valid=np.array(X[split_idx_train:split_idx_valid])
@@ -26,7 +27,7 @@ X0_valid=np.array([X1_valid**0])
 X2_valid=np.array([X1_valid**2])
 X3_valid=np.array([X1_valid**3])
 X4_valid=np.array([X1_valid**4])
-Y_valid=np.array([populationGenerator(X1_valid)])
+Y_valid=np.array([Y[split_idx_train:split_idx_valid]])
 
 
 X1_test=np.array(X[split_idx_valid:])
@@ -34,28 +35,28 @@ X0_test=np.array([X1_test**0])
 X2_test=np.array([X1_test**2])
 X3_test=np.array([X1_test**3])
 X4_test=np.array([X1_test**4])
-Y_test=np.array([populationGenerator(X1_test)])
+Y_test=np.array(Y[split_idx_valid:])
 
 
 Xtrans=np.array([X0_train, X1_train, X2_train, X3_train, X4_train])
-X=np.transpose(Xtrans)
-XInv=np.linalg.inv(np.matmul(Xtrans, X))
+X_y=np.transpose(Xtrans)
+XInv=np.linalg.inv(np.matmul(Xtrans, X_y))
 calc1=np.matmul(XInv, Xtrans)
 beta=calc1=np.matmul(calc1, Y_train)
 
-def linear_model(X1:list[float], beta:list[float])->list[float]:
+def linearModel(X1:list[float], beta:list[float])->list[float]:
     return beta[0]+(beta[1]*X1)
 
-def quadratic_model(X1, X2, beta):
+def quadraticModel(X1:list[float], X2:list[float], beta:list[float])->list[float]:
     return beta[0]+(beta[1]*X1)+(beta[2]*X2)
 
-def cubic_model(X1, X2, X3, beta):
+def cubicModel(X1:list[float], X2:list[float], X3:list[float], beta:list[float])->list[float]:
     return beta[0]+(beta[1]*X1)+(beta[2]*X2)+(beta[3]*X3)
 
-def quarternary_model(X1, X2, X3, X4, beta):
-    return beta[0]+(beta[1]*X1)+(beta[2]*X2)+(beta[3]*X3)+(+(beta[4]*X4))
+def quarternaryModel(X1:list[float], X2:list[float], X3:list[float], X4:list[float], beta:list[float])->list[float]:
+    return beta[0]+(beta[1]*X1)+(beta[2]*X2)+(beta[3]*X3)+((beta[4]*X4))
 
-def lagrangesPolynomial(X, Y, xi, n):
+def lagrangesPolynomial(X:list[float], Y:list[float], xi:int, n:int)->int:
     res=0.0
     for i in range(n):
         t=Y[i]
@@ -65,11 +66,45 @@ def lagrangesPolynomial(X, Y, xi, n):
         res+=t
     return res
 
-def train():
-    lin_model_train=linear_model(X1_train, beta)
-    quad_model_train=quadratic_model(X1_train, X2_train, beta)
-    cub_model_train=cubic_model(X1_train, X2_train, X3_train, beta)
-    quart_model_train=quarternary_model(X1_train, X2_train, X3_train, X4_train, beta)
+def models(X:list[float], Y:list[float], beta:list[float])->list[list[float]]:
+    X1=X
+    X2=np.array(X1**2)
+    X3=np.array(X1**3)
+    X4=np.array(X1**4)
+    lin_model=linearModel(X1, beta)
+    quad_model=quadraticModel(X1, X2, beta)
+    cub_model=cubicModel(X1, X2, X3, beta)
+    quart_model=quarternaryModel(X1, X2, X3, X4, beta)
+    lagranges_model=[lagrangesPolynomial(X1,Y,X1[i],len(X1)) for i in range(len(X1))]
+    mod=[lin_model, quad_model, cub_model, quart_model, lagranges_model]
+    return mod
+
+models=models(X, Y, beta)
+
+
+def plot(X:list[float], Y:list[float], models:list[float])->int:
+    txt="Plot that shows the performance of the Linear, quadratic, cubic, biquadratic and lagrange's polynomial model for the given inputs and outputs"
+    plt.scatter(X, Y, marker=".", label="Actual Values")
+    plt.title("Model Performance Estimation(Linear, Quadratic, Cubic, Quarternary, Lagrange's polynomial)")
+    plt.plot(X, models[0], label="Linear polynomial model")
+    plt.plot(X, models[1], label="Quadratic polynomial model")
+    plt.plot(X, models[2], label="Cubic polynomial model")
+    plt.plot(X, models[3], label="Quarternary polynomial model")
+    plt.plot(X, models[4], label="Lagrange's polynomial model", c="r")
+    plt.xlabel("Feature values")
+    plt.ylabel("Output values")
+    plt.figtext(0.5, 0.01, txt, wrap=True, horizontalalignment='center', fontsize=8)
+    plt.legend()
+    plt.show()
+    plt.close()
+    return 1
+plot(X,Y,models)
+
+def train(X1_train:list[float], X2_train:list[float], X3_train:list[float], X4_train:list[float], beta:list[float])->list[float]:
+    lin_model_train=linearModel(X1_train, beta)
+    quad_model_train=quadraticModel(X1_train, X2_train, beta)
+    cub_model_train=cubicModel(X1_train, X2_train, X3_train, beta)
+    quart_model_train=quarternaryModel(X1_train, X2_train, X3_train, X4_train, beta)
     lagranges_model_train=[lagrangesPolynomial(X1_train,Y_train,X1_train[i],len(X1_train)) for i in range(len(X1_train))]
 
     eps_linear_model_train=np.sum(np.abs(Y_train-lin_model_train))/len(X1_train)
@@ -77,15 +112,16 @@ def train():
     eps_cub_model_train=np.sum(np.abs(Y_train-cub_model_train))/len(X1_train)
     eps_quart_model_train=np.sum(np.abs(Y_train-quart_model_train))/len(X1_train)
     eps_lag_model_train=np.sum(np.abs(Y_train-lagranges_model_train))/len(X1_train)
-    return [eps_linear_model_train, eps_quad_model_train, eps_cub_model_train, eps_quart_model_train, eps_lag_model_train]
+    op=[eps_linear_model_train, eps_quad_model_train, eps_cub_model_train, eps_quart_model_train, eps_lag_model_train]
+    return op
 
-eps_bias=train()
+eps_bias=train(X1_train, X2_train, X3_train, X4_train, beta)
 
-def valid():
-    lin_model_valid=linear_model(X1_valid, beta)
-    quad_model_valid=quadratic_model(X1_valid, X2_valid, beta)
-    cub_model_valid=cubic_model(X1_valid, X2_valid, X3_valid, beta)
-    quart_model_valid=quarternary_model(X1_valid, X2_valid, X3_valid, X4_valid, beta)
+def valid(X1_valid:list[float], X2_valid:list[float], X3_valid:list[float], X4_valid:list[float], beta:list[float])->list[float]:
+    lin_model_valid=linearModel(X1_valid, beta)
+    quad_model_valid=quadraticModel(X1_valid, X2_valid, beta)
+    cub_model_valid=cubicModel(X1_valid, X2_valid, X3_valid, beta)
+    quart_model_valid=quarternaryModel(X1_valid, X2_valid, X3_valid, X4_valid, beta)
     # lagranges_model_valid=[lagrangesPolynomial(X1_valid,Y_valid,X1_valid[i],70) for i in range(len(X1_train))]
 
     eps_linear_model_valid=np.sum(np.abs(Y_valid-lin_model_valid))/len(X1_valid)
@@ -96,18 +132,19 @@ def valid():
     # eps_lag_model_valid=np.sum(np.abs(Y_valid-lagranges_model_valid))
     return [eps_linear_model_valid, eps_quad_model_valid, eps_cub_model_valid, eps_quart_model_valid] #eps_lag_model_valid
 
-eps_variance=valid()
+eps_variance=valid(X1_valid, X2_valid, X3_valid, X4_valid, beta)
 
 print(eps_bias)
 print(eps_variance)
 
+
 txt="This plot presents the Bias-variance trade off for the Linear, Quadratic, Cubic, Quarternary models and a Lagrange's Polynomial"
 x=[1,2,3,4,70]
-plt.plot(x, eps_bias, c="r", label="Bias")
+plt.plot(x, eps_bias, c="r", label="Bias", marker=".")
 plt.title("Bias-Variance Trade off")
 plt.xlabel("Model Complexity")
 plt.ylabel("Error estimate")
 plt.figtext(0.5, 0.01, txt, wrap=True, horizontalalignment='center', fontsize=8)
-plt.plot(x[:4], eps_variance, c="b", label="Variance")
+plt.plot(x[:4], eps_variance, c="b", label="Variance", marker=".")
 plt.legend()
 plt.show()