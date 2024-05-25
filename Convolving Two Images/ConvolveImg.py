5fab56e3ee8d39d2de754f4d0415382e428a3d11
gdeeeeyy <gd14405@gmail.com>
2024-05-25 19:31:57 +0530

    some new changes, still have to work on either 2d or 3d convolution

--- Convolving Two Images/ConvolveImg.py
+++ Convolving Two Images/ConvolveImg.py
@@ -1,36 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
-#import cv2
 
 img=mpimg.imread("Convolving Two Images/img.JPG")
-img=np.array(img).reshape(img, (300, 369))
+sp1=img.shape
+print(sp1)
 plt.imshow(img)
 plt.show()
 
 ker=mpimg.imread("Convolving Two Images/kernel.png")
-ker=np.array(ker).reshape(3,3)
 plt.imshow(ker)
 plt.show()
 
-pixels=[]
-for i in range(360):
-    for j in range(300):
-        pixels.append(img[i, j])
 
-pixels=np.array(pixels).reshape(360, 300)
-
-conv_mat=[]
-for i in range(1,358):
-    for j in range(1,298):
-        temp=pixels[i:i+3, j:j+3]
-        pdt=np.multiply(temp, ker)
-        sumAfterPdt=np.sum(pdt)
-        if(sumAfterPdt>=255):
-            pdt=255
-        elif(sumAfterPdt<0):
-            pdt=0
-        conv_mat.append(sumAfterPdt)
-
-conv_mat=np.array(conv_mat).reshape(357, 297)
-print(conv_mat)
+# don't try to convert the image into a matrix, its already a matrix type thingy, so just try to use this matrix
+# the range should be x and y coords of the shape tuple
