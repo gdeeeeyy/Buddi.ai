675db5d96a4b40537b0a57f279263d20708c30fe
gdeeeeyy <gd14405@gmail.com>
2024-05-26 22:54:42 +0530

    still have to find a way to convolve these two images

--- Convolving Two Images/ConvolveImg.py
+++ Convolving Two Images/ConvolveImg.py
@@ -4,14 +4,18 @@ import matplotlib.image as mpimg
 
 img=mpimg.imread("Convolving Two Images/img.JPG")
 sp1=img.shape
-print(sp1)
+print(img)
 plt.imshow(img)
-plt.show()
+#plt.show()
 
 ker=mpimg.imread("Convolving Two Images/kernel.png")
 plt.imshow(ker)
-plt.show()
+#plt.show()
 
+#np.convolve
+
+#convolution is mostly integration
+#y[i,j]=[sum(m=- to +inf) sum(n=- to +inf){h[m,n].x[i-m, j-n]}]
 
 # don't try to convert the image into a matrix, its already a matrix type thingy, so just try to use this matrix
 # the range should be x and y coords of the shape tuple
