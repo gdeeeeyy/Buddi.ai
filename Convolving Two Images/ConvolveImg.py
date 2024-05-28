334a67ab24a19737bfd87d56b3a26bd51ed5cfd9
gdeeeeyy <gd14405@gmail.com>
2024-05-28 11:30:11 +0530

    made some changes, its working better rn

--- Convolving Two Images/ConvolveImg.py
+++ Convolving Two Images/ConvolveImg.py
@@ -3,32 +3,34 @@ import matplotlib.pyplot as plt
 from PIL import Image
 import scipy.signal as sp
 
-img=Image.open("Convolving Two Images/img.JPG")
+#opening the image
+img=Image.open("Convolving Two Images/img.jpg")
+#converting the image to b&w
 imgBw=img.convert("1")
+#plotting the image
 plt.imshow(imgBw)
 plt.show()
 
+#converting the image into a numpy array
 arrayImg=np.array(imgBw, dtype=np.float64)
 print(arrayImg.shape)
 
-kerImg=Image.open("Convolving Two Images/kernel.png")
+#opening the kernel image
+kerImg=Image.open("Convolving Two Images/ker2.png")
+#converting the kernel to b&w
 kerBw=kerImg.convert("1")
+#plotting the kernel
 plt.imshow(kerBw)
 plt.show()
 
+#converting the kernel image to an array
 arrayKer=np.array(kerBw, dtype=np.float64)
 print(arrayKer.shape)
 
-res=sp.convolve2d(arrayImg, arrayKer, mode='same', boundary='fill', fillvalue=0)
+#convolving the image and the kernel
+res=sp.convolve2d(arrayImg, arrayKer, mode='valid', boundary='wrap')
 print(res.shape)
 
+#plotting the output of the convolution
 plt.imshow(res)
 plt.show()
-
-
-
-#convolution is mostly integration
-#y[i,j]=[sum(m=- to +inf) sum(n=- to +inf){h[m,n].x[i-m, j-n]}]
-
-# don't try to convert the image into a matrix, its already a matrix type thingy, so just try to use this matrix
-# the range should be x and y coords of the shape tuple
